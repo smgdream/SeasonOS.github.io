f5bdc822336e08d14bfcdbdbf3762bf24faafa28
smg <53500312+smgdream@users.noreply.github.com>
2021-04-04 22:49:13 +0800

    Add files via upload

--- download/OdyinkServer.bat
+++ download/OdyinkServer.bat
@@ -1,10 +1,12 @@
 @echo off
-rem 4.0
+rem 5.0
 rem 初始化
     chcp 936 >nul
     title Odyink Server
     color 07
     cd /d %~dp0
+    cls
+    setlocal enabledelayedexpansion
 rem 检测安装
     :Check
     if not exist odyink\Bloglist.bat goto :install
@@ -23,7 +25,7 @@ rem 菜单
     if "%Munum%"=="1" goto :VB
     if "%Munum%"=="2" goto :cpBlog
     if "%Munum%"=="3" goto :DBlog
-    if "%Munum%"=="4" exit
+    if "%Munum%"=="4" ( cls & exit )
     echo 输入无效
     timeout /t 2 /nobreak >nul
     goto :menu
@@ -39,104 +41,108 @@ rem 校阅文章
         echo 目前有%Blognum%篇文章
         echo.
         echo q.返回主页
-        set EBlognum=
-        set /p EBlognum=文章序号：
+        set inputBlogNum=
+        set /p inputBlogNum=文章序号：
         cls
-        if /i "%EBlognum%"=="q" goto :menu
-    rem 预处理
-        :NBBlog
-        set Doctype=
-    rem 检测文章是否存在
-        if not exist Blog\"%EBlognum%.bat" (
-            if not exist Blog\"%EBlognum%.txt" (
-                echo 文章不存在
-                timeout /t 2 /nobreak >nul
-                goto :VB
+        if /i "%inputBlogNum%"=="q" goto :menu
+    rem 浏览文章
+        rem 预处理
+            :NBBlog
+            set Doctype=
+        rem 检测文章是否存在
+            if not exist Blog\"%inputBlogNum%.bat" (
+                if not exist Blog\"%inputBlogNum%.txt" (
+                    echo 文章不存在
+                    timeout /t 2 /nobreak >nul
+                    goto :VB
+                )
             )
-        )
-    rem 显示文本内容Batch
-        rem 因复合句中变量为复合句前的变量使用用延迟变量获得句中变量的动态值
-        setlocal enabledelayedexpansion
-        if exist Blog\"%EBlognum%.bat" (
-            set Doctype=bat
-            echo 这是Batch扩展
-            echo 可在odyink\Blog\%EBlognum%.bat中查看代码
-            echo 因Batch扩展特殊性执行后果自负
-            echo 查看代码是为了防病毒!!!
-            set batruncode=
-            echo y.确认执行 c.编辑代码 n.取消执行
-            set /p batruncode=操作序号:
-            if /i "!batruncode!"=="c" (
-                notepad.exe .\Blog\%EBlognum%.%Doctype%
+        rem 显示文本内容Batch
+            rem 因复合句中变量为复合句前的变量使用用延迟变量获得句中变量的动态值
+            if exist Blog\"%inputBlogNum%.bat" (
+                set Doctype=bat
+                echo 这是Batch扩展
+                echo 可在odyink\Blog\%inputBlogNum%.bat中查看代码
+                echo 因Batch扩展特殊性执行后果自负
+                echo 查看代码是为了防病毒!!!
+                echo.
+                echo y.确认执行 c.编辑代码 e.取消执行
+                echo   b.上一篇 q.返回列表 n.下一篇
+                set batruncode=
+                set /p batruncode=操作序号:
+                if /i "!batruncode!"=="b" goto :backBlog
+                if /i "!batruncode!"=="n" goto :nextBlog
+                if /i "!batruncode!"=="c" (
+                    notepad.exe .\Blog\%inputBlogNum%.%Doctype%
+                    cls
+                    goto :NBBlog
+                )
+                if /i "!batruncode!"=="y" (
+                    cls
+                    cmd /c .\Blog\%inputBlogNum%.bat
+                    cls
+                    rem 重新初始化
+                        rem 这里if复合句不能用@echo off会报错现移到:BlogconNE下一行
+                        rem 这里if复合句不用cd，它会自动恢复(原因不明)而且用cd会报错
+                        chcp 936 >nul
+                        title Odyink Server
+                        color 07
+                    echo b.上一篇 q.返回列表 n.下一篇
+                    echo          c.编辑代码
+                    goto :BlogconNE
+                ) else (
+                    goto :VB
+                )
+            )
+        rem 显示文本内容Text
+            set Doctype=txt
+            type Blog\%inputBlogNum%.txt
+            echo.
+            echo.
+            echo b.上一篇 q.返回列表 n.下一篇
+            echo          c.修改文章
+            echo.
+        rem 文章操作
+            :BlogconNE
+            @echo off
+            set Blogcon=
+            set /p Blogcon=操作序号：
+            if /i "%Blogcon%"=="b" goto :backBlog
+            if /i "%Blogcon%"=="n" goto :nextBlog
+            if /i "%Blogcon%"=="q" goto :VB
+            if /i "%Blogcon%"=="c" (
+                notepad.exe .\Blog\%inputBlogNum%.%Doctype%
                 cls
                 goto :NBBlog
             )
-            if /i "!batruncode!"=="y" (
-                cls
-                cmd /c .\Blog\%EBlognum%.bat
-                cls
-                rem 重新初始化
-                    rem 这里if复合句不能用@echo off会报错现移到:BlogconNE下一行
-                    rem 这里if复合句不用cd，它会自动恢复(原因不明)而且用cd会报错
-                    chcp 936 >nul
-                    title Odyink User
-                    color 07
-                echo b.上一篇 q.返回列表 n.下一篇
-                echo          c.编辑代码
-                goto :BlogconNE
-            ) else (
-                goto :VB
-            )
-        )
-    rem 显示文本内容Text
-        set Doctype=txt
-        type Blog\%EBlognum%.txt
-        echo.
-        echo.
-        echo b.上一篇 q.返回列表 n.下一篇
-        echo          c.修改文章
-        echo.
-    rem 文章操作
-        :BlogconNE
-        @echo off
-        set Blogcon=
-        set /p Blogcon=操作序号：
-        if /i "%Blogcon%"=="b" goto :backBlog
-        if /i "%Blogcon%"=="n" goto :nextBlog
-        if /i "%Blogcon%"=="q" goto :VB
-        if /i "%Blogcon%"=="c" (
-            notepad.exe .\Blog\%EBlognum%.%Doctype%
+            echo 输入无效
+            echo.
+            goto :BlogconNE
+        rem 上一篇文章
+            :backBlog
+            set StartinputBlogNum=%inputBlogNum%
             cls
-            goto :NBBlog
-        )
-        echo 输入无效
-        echo.
-        goto :BlogconNE
-    rem 上一篇文章
-        :backBlog
-        set StartEBlognum=%EBlognum%
-        cls
-        :Back
-        set /a EBlognum=%EBlognum%-1
-        set /a AV=%StartEBlognum%-%EBlognum%
-        rem 两文章间距不可大于100
-        if %AV%==100 goto :NBBlog
-        if %EBlognum%==-1 goto :NBBlog
-        if exist Blog\%EBlognum%.bat goto :NBBlog
-        if exist Blog\%EBlognum%.txt goto :NBBlog
-        goto :Back
-    rem 下一篇文章
-        :nextBlog
-        set StartEBlognum=%EBlognum%
-        cls
-        :Next
-        set /a EBlognum=%EBlognum%+1
-        set /a AV=%EBlognum%-%StartEBlognum%
-        rem 两文章间距不可大于100
-        if %AV%==100 goto :NBBlog
-        if exist Blog\%EBlognum%.bat goto :NBBlog
-        if exist Blog\%EBlognum%.txt goto :NBBlog
-        goto :Next
+            :Back
+            set /a inputBlogNum=%inputBlogNum%-1
+            set /a AV=%StartinputBlogNum%-%inputBlogNum%
+            rem 两文章间距不可大于100
+            if %AV%==100 goto :NBBlog
+            if %inputBlogNum%==-1 goto :NBBlog
+            if exist Blog\%inputBlogNum%.bat goto :NBBlog
+            if exist Blog\%inputBlogNum%.txt goto :NBBlog
+            goto :Back
+        rem 下一篇文章
+            :nextBlog
+            set StartinputBlogNum=%inputBlogNum%
+            cls
+            :Next
+            set /a inputBlogNum=%inputBlogNum%+1
+            set /a AV=%inputBlogNum%-%StartinputBlogNum%
+            rem 两文章间距不可大于100
+            if %AV%==100 goto :NBBlog
+            if exist Blog\%inputBlogNum%.bat goto :NBBlog
+            if exist Blog\%inputBlogNum%.txt goto :NBBlog
+            goto :Next
 rem 导入文章
     :cpBlog
     cls
@@ -177,12 +183,15 @@ rem 导入文章
         if "%BlogAnum:~-1%"==" " set BlogAnum=%BlogAnum:~0,-1%
         set /a NewBlogAnum=%BlogAnum%+1
         rem 写入
-        echo set Blognum=%NewBlognum% >Blognum.bat
-        echo set BlogAnum=%NewBlogAnum% >BlogAnum.bat
-        echo set NEB%NewBlogAnum%=E>>Blogexist.bat
-        echo if not %%NEB%NewBlogAnum%%%==Del echo %NewBlogAnum%.%Blogtitle% >>Bloglist.bat
-        echo %date:~0,-2%%time% copy %Docname% to .\odyink\Blog\ (num:%NewBlogAnum%) >>Bloglog.log
-        if exist %Docname% copy %Docname% Blog\%NewBlogAnum%.%Doctype% >nul
+            rem 写入新配置
+                echo set Blognum=%NewBlognum% >Blognum.bat
+                echo set BlogAnum=%NewBlogAnum% >BlogAnum.bat
+                echo set NEB%NewBlogAnum%=E>>Blogexist.bat
+                echo if not %%NEB%NewBlogAnum%%%==Del echo %NewBlogAnum%.%Blogtitle% >>Bloglist.bat
+            rem 写入日志
+                echo %date:~0,-2%%time% copy %Docname% to .\odyink\Blog\ (num:%NewBlogAnum%) >>Bloglog.log
+            rem 复制文章
+                if exist %Docname% copy %Docname% Blog\%NewBlogAnum%.%Doctype% >nul
         echo 导入完毕
         timeout /t 2 /nobreak >nul
         goto :cpBlog
@@ -231,7 +240,7 @@ rem 安装
     echo 回车安装Odyink
     pause >nul
     cls
-    mkdir odyink\Blog >nul
+    mkdir odyink\Blog 2>nul
     cd Odyink
     if exist Blog\*.*t del /f /s /q Blog\*.*t >nul
     rem 新建文件写入信息
@@ -244,10 +253,8 @@ rem 安装
         echo rem Blogdel>Blogdel.bat
         echo [Bloglog]>Bloglog.log
         echo Odyink是由Andy(python)和SMG(Batch)制作的命令行个人博客软件 >>Blog\0.txt
-        rem 为了兼容老版本Odyink User
+    rem 为了兼容老版本Odyink User
         echo ServerOK >ServerOK
-        cd ..\
-        echo 安装完毕
-        timeout /t 2 /nobreak >nul
-        cls
-        goto :Check
+    echo 安装完毕
+    timeout /t 2 /nobreak >nul
+    %0
--- download/OdyinkUser.bat
+++ download/OdyinkUser.bat
@@ -1,33 +1,23 @@
 @echo off
-rem 4.0
-rem 初始化环境
+rem 5.0
+rem 初始化
     chcp 936 >nul
     title Odyink User
     color 07
     cd /d %~dp0
+    cls
+    setlocal enabledelayedexpansion
 rem 检测
     :check
     rem 检测配置
         if not exist odyink\website.bat goto :setOdyink
         cd odyink\
     rem 检测设置下载器
-        set wgetparameter=
-        set psparameter=
-        set ps=
-        if exist ..\wget.exe set wget=..\wget.exe
-        if exist .\wget.exe set wget=wget.exe
-        if exist %SystemRoot%\System32\wget.exe set wget=wget.exe
-        if exist %SystemRoot%\SysWOW64\wget.exe set wget=wget.exe
-        if %wget:~-8%==wget.exe (
-            rem -q前有1空格
-            rem win10可用https而7不行为兼容性不查证书(不安全)
-            set wgetparameter= -q --no-check-certificate -P ./
-        ) else (
-            set ps=use
-            set wget=PowerShell Invoke-WebRequest
-            rem -outfile 前后各有1空格
-            set psparameter= -outfile 
-        )
+        if exist ..\wget.exe copy ..\wget.exe .\wget.exe
+        rem 为兼容性不查证书(不安全)
+        set WO=-q --no-check-certificate -P ./
+        set pswget=PowerShell Invoke-WebRequest
+        set PSO=-outfile
 rem 看文章
     rem 下载列表
         :get
@@ -35,28 +25,28 @@ rem 看文章
         echo 正在下载列表请稍后...
         call website.bat
         if "%website:~-1%"==" " set website=%website:~0,-1%
-        if exist index.html del index.html
-        if exist Blog*.bat del Blog*.bat
-        if exist exdoc.bat del exdoc.bat
-        if exist Blog\*.*t del /f /s /q Blog\*.*t >nul
-        echo 正在下载Bloglist.bat
-        if "%ps%"=="use" set psdf=Bloglist.bat
-        %wget%%wgetparameter% %website%/Bloglist.bat%psparameter%%psdf%
-        if not exist Bloglist.bat goto :ServerError
-        echo 正在下载Blognum.bat
-        if "%ps%"=="use" set psdf=Blognum.bat
-        %wget%%wgetparameter% %website%/Blognum.bat%psparameter%%psdf%
-        echo 正在下载Blogexist.bat
-        if "%ps%"=="use" set psdf=Blogexist.bat
-        %wget%%wgetparameter% %website%/Blogexist.bat%psparameter%%psdf%
-        echo 正在下载Blogdel.bat
-        if "%ps%"=="use" set psdf=Blogdel.bat
-        %wget%%wgetparameter% %website%/Blogdel.bat%psparameter%%psdf%
-        if exist ..\.wget-hsts del ..\.wget-hsts
-        if not exist Blognum.bat goto :ServerError
-        if not exist Bloglist.bat goto :ServerError
-        if not exist Blogdel.bat goto :ServerError
-        if not exist Blogexist.bat goto :ServerError
+        rem 删除旧文件
+            if exist index.html del index.html
+            if exist Blog*.bat del Blog*.bat
+            if exist exdoc.bat del exdoc.bat
+            if exist Blog\*.*t del /f /s /q Blog\*.*t >nul
+        rem 下载新列表文件
+            echo 正在下载Bloglist.bat
+                wget %WO% %website%/Bloglist.bat 2>nul
+                if not exist Bloglist.bat %pswget% %website%/Bloglist.bat %PSO% Bloglist.bat >nul
+                if not exist Bloglist.bat goto :ServerError
+            echo 正在下载Blognum.bat
+                wget %WO% %website%/Blognum.bat 2>nul
+                if not exist Blognum.bat %pswget% %website%/Blognum.bat %PSO% Blognum.bat >nul
+                if not exist Blognum.bat goto :ServerError
+            echo 正在下载Blogexist.bat
+                wget %WO% %website%/Blogexist.bat 2>nul
+                if not exist Blogexist.bat %pswget% %website%/Blogexist.bat %PSO% Blogexist.bat >nul
+                if not exist Blogexist.bat goto :ServerError
+            echo 正在下载Blogdel.bat
+                wget %WO% %website%/Blogdel.bat 2>nul
+                if not exist Blogdel.bat %pswget% %website%/Blogdel.bat %PSO% Blogdel.bat >nul
+                if not exist Blogdel.bat goto :ServerError
         cls
     rem 文章列表
         :VB
@@ -69,35 +59,31 @@ rem 看文章
         echo 目前有%Blognum%篇文章
         echo.
         echo r.刷新 q.退出
-        set EBlognum=
-        set /p EBlognum=文章序号：
+        echo u.检查更新
+        set inputBlogNum=
+        set /p inputBlogNum=文章序号：
         cls
-        if /i "%EBlognum%"=="r" goto :get
-        if /i "%EBlognum%"=="q" goto :exit
+        if /i "%inputBlogNum%"=="r" goto :get
+        if /i "%inputBlogNum%"=="q" goto :exit
+        if /i "%inputBlogNum%"=="u" goto :update
     rem 下载浏览文章
         rem 预处理
             :NBBlog
             cls
-            set NEBEB=
-            if "%EBlognum:~-1%"==" " set EBlognum=%EBlognum:~0,-1%
+            set findExistBlog=
+            if "%inputBlogNum:~-1%"==" " set inputBlogNum=%inputBlogNum:~0,-1%
             if exist Blog\*.*t del /f /s /q Blog\*.*t >nul
             cls
         rem 下载文章
             echo 下载文章中
-            rem 改输出目录为./Blog/
-            rem 因正常bat和txt中只存在一者,至少一条报错用nul屏蔽
-            if %wget:~-8%==wget.exe set wgetparameter= -q --no-check-certificate -P ./Blog/
-            if "%ps%"=="use" set psdf=Blog\%EBlognum%.bat
-            %wget%%wgetparameter% %website%/Blog/"%EBlognum%.bat"%psparameter%%psdf% >nul
-            if "%ps%"=="use" set psdf=Blog\%EBlognum%.txt
-            %wget%%wgetparameter% %website%/Blog/"%EBlognum%.txt"%psparameter%%psdf% >nul
-            if exist ..\.wget-hsts del ..\.wget-hsts
-            rem 改回目录为./
-            if %wget:~-8%==wget.exe set wgetparameter= -q --no-check-certificate -P ./
+                wget %WO%Blog/ %website%/Blog/%inputBlogNum%.bat 2>nul
+                if not exist %inputBlogNum%.bat %pswget% %website%/Blog/%inputBlogNum%.bat %PSO% .\Blog\%inputBlogNum%.bat >nul
+                wget %WO%Blog/ %website%/Blog/%inputBlogNum%.txt 2>nul
+                if not exist %inputBlogNum%.txt %pswget% %website%/Blog/%inputBlogNum%.txt %PSO% .\Blog\%inputBlogNum%.txt >nul
             cls
         rem 检测文章是否存在
-        if not exist Blog\"%EBlognum%.bat" (
-            if not exist Blog\"%EBlognum%.txt" (
+        if not exist Blog\"%inputBlogNum%.bat" (
+            if not exist Blog\"%inputBlogNum%.txt" (
                 echo 文章不存在
                 timeout /t 2 /nobreak >nul
                 goto :VB
@@ -105,33 +91,36 @@ rem 看文章
         )
         rem 显示文本内容Batch
             rem 因复合句中变量为复合句前的变量,使用用延迟变量获得句中变量的动态值
-            setlocal enabledelayedexpansion
-            if exist Blog\"%EBlognum%.bat" (
+            if exist Blog\"%inputBlogNum%.bat" (
                 echo 这是Batch扩展
-                echo 可在odyink\Blog\%EBlognum%.bat中查看代码
+                echo 可在odyink\Blog\%inputBlogNum%.bat中查看代码
                 echo 因Batch扩展特殊性执行后果自负
                 echo 查看代码是为了防病毒!!!
-                set batrunyn=
-                set /p batrunyn=是否确认执行yn:
-                if /i "!batrunyn!"=="y" (
+                echo.
+                echo y.确认执行 e.取消执行
+                echo   b.上一篇 n.下一篇
+                set batruncode=
+                set /p batruncode=操作序号:
+                if /i "!batruncode!"=="b" goto :backBlog
+                if /i "!batruncode!"=="n" goto :nextBlog
+                if /i "!batruncode!"=="y" (
                     cls
-                    cmd /c .\Blog\%EBlognum%.bat
+                    cmd /c .\Blog\%inputBlogNum%.bat
                     cls
                     rem 重新初始化
-                        rem 这里if复合句不能用@echo off会报错现移到:BlogconNE下一行
-                        rem 这里if复合句不用cd，它会自动恢复(原因不明)而且用cd会报错
+                        rem 这里if复合句不用@echo off和cd，它会自动恢复(原因不明)一用就会报错
                         chcp 936 >nul
                         title Odyink User
                         color 07
                     echo b.上一篇 q.返回列表 n.下一篇
-                    echo         r.刷新扩展
+                    echo          r.刷新扩展
                     goto :BlogconNE
                 ) else (
                     goto :VB
                 )
             )
         rem 显示文本内容Text
-            type Blog\%EBlognum%.txt
+            type Blog\%inputBlogNum%.txt
             echo.
             echo.
             echo b.上一篇 q.返回列表 n.下一篇
@@ -139,7 +128,6 @@ rem 看文章
             echo.
         rem 文章操作
             :BlogconNE
-            @echo off
             set Blogcon=
             set /p Blogcon=操作序号：
             if /i "%Blogcon%"=="b" goto :backBlog
@@ -151,47 +139,85 @@ rem 看文章
             goto :BlogconNE
         rem 上一篇文章
             :backBlog
-            set StartEBlognum=%EBlognum%
+            set StartinputBlogNum=%inputBlogNum%
             cls
             echo 正在检索请稍后...
             :Back
-            set /a EBlognum=%EBlognum%-1
-            set /a AV=%StartEBlognum%-%EBlognum%
-            call set NEBEB=%%NEB%EBlognum%%%
-            if "%NEBEB%"=="E" goto :NBBlog
+            set /a inputBlogNum=%inputBlogNum%-1
+            set /a AV=%StartinputBlogNum%-%inputBlogNum%
+            call set findExistBlog=%%NEB%inputBlogNum%%%
+            if "%findExistBlog%"=="E" goto :NBBlog
             rem 两文章间距不可大于100
             if %AV%==100 goto :NBBlog
             goto :Back
         rem 下一篇文章
             :nextBlog
-            set StartEBlognum=%EBlognum%
+            set StartinputBlogNum=%inputBlogNum%
             cls
             echo 正在检索请稍后...
             :Next
-            set /a EBlognum=%EBlognum%+1
-            set /a AV=%EBlognum%-%StartEBlognum%
-            call set NEBEB=%%NEB%EBlognum%%%
-            if "%NEBEB%"=="E" goto :NBBlog
+            set /a inputBlogNum=%inputBlogNum%+1
+            set /a AV=%inputBlogNum%-%StartinputBlogNum%
+            call set findExistBlog=%%NEB%inputBlogNum%%%
+            if "%findExistBlog%"=="E" goto :NBBlog
             rem 两文章间距不可大于100
             if %AV%==100 goto :NBBlog
             goto :Next
+rem 检查更新
+    :update
+    cd ..\
+    if exist Update.bat del /f Update.bat
+    rem 设置更新源(结尾无斜杠)
+    set updateWebsite=https://odyink-1302226504.cos.accelerate.myqcloud.com
+    echo 正在检查更新
+    wget %WO% %updateWebsite%/Update.bat 2>nul
+    if not exist Update.bat %pswget% %updateWebsite%/Update.bat %PSO% Update.bat >nul
+    cls
+    if not exist Update.bat (
+        echo 检查更新出错
+        timeout /t 2 /nobreak >nul
+        cls
+        goto :VB
+    )
+    set updateYN=
+    call Update.bat
+    rem 当前版本(整数)
+    set version=5
+    if %latestVersion% gtr %version% (
+        echo 存在最新版本%latestVersion%
+        echo 输入y确认更新其他键退出
+        set /p updateYN=请输入:
+        if /i "!updateYN!"=="y" (
+            rem y是参数%1 %updateWebsite%是参数%2 %~nx1(包含后缀名的文件名)是参数%3
+            start Update.bat y %updateWebsite% %~nx0
+            exit
+            ) else (
+                if exist Update.bat del /f Update.bat
+                cd .\odyink\
+                set updateYN=
+                goto :VB
+            )
+    ) else (
+        echo 当前已是最新版本
+        timeout /t 2 /nobreak >nul
+        cls
+        goto :VB
+    )
 rem 配置Odyink
     :setOdyink
     echo 配置Odyink
     set /p website=域名或IP[/目录]：
     echo 正在配置Odyink...
     if "%website:~-1%"=="/" set website=%website:~0,-1%
-    mkdir odyink\Blog >nul
+    mkdir odyink\Blog 2>nul
     cd odyink
     if exist Blog*.bat del Blog*.bat
     if exist Blog\*.*t del /f /s /q Blog\*.*t >nul
     echo set website=%website% >website.bat
-    cd ..\
     cls
     echo 配置完毕
     timeout /t 2 /nobreak >nul
-    cls
-    goto :check
+    %0
 rem 退出
     :ServerError
     echo 服务器出错
@@ -201,4 +227,5 @@ rem 退出
     if exist Blog*.bat del Blog*.bat
     if exist Blog\*.*t del /f /s /q Blog\*.*t >nul
     timeout /t 2 /nobreak >nul
+    cls
     exit
